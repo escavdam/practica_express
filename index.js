c5452f09cb34d094b6a8263210061c240b5c9315
Arkant04 <delacorte19@gmail.com>
2024-02-08 14:50:18 +0100

    MOD: se añade una funcion que hace que aparezcan emojis random (solo falta comentar el issue 4)

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ app.get(`/`, (req, res) => {
 
 
 app.get(`/emoji`, (req, res) => {
-  
+  const emoji = ['🍇', '🍈', '🍉', '🍊', '🍋', '🍌', '🍍', '🍎', '🍏', '🍐', '🍑', '🍒', '🍓', '🥝', '🍅', '🥥', '🥑', '🍆', '🥔', '🥕']
+
+  const randomEmoji = emoji[Math.floor(Math.random() * emoji.length)];
+  res.send(randomEmoji)
 });
 
 app.listen(3000, () => {
