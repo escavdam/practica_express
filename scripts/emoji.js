203031d56ad764229ce98057e47439bd7c5ef843
Álvaro <17alma41@gmail.com>
2024-02-12 21:51:16 +0100

    emoji-aleatorio

---
+++ scripts/emoji.js
@@ -0,0 +1,15 @@
+function randomEmoji(){
+    const emojis = ['🍇', '🍈', '🍉', '🍊', '🍋', '🍌', '🍍', '🍎', '🍏', '🍐', '🍑', '🍒', '🍓', '🥝', '🍅', '🥥', '🥑', '🍆', '🥔', '🥕']
+    const emoji = emojis[Math.floor(Math.random() * emojis.length)];
+    return emoji;
+}
+
+function multipleEmoji(number){
+    const emojis = []
+    for(let i = 0; i < number; i++){
+        emojis.push(randomEmoji())
+    }
+    return emojis
+}
+
+module.exports = { randomEmoji, multipleEmoji }
